3cbd4b82b21be81eb6ef234e4d4bc3965bcf633c
Mauricio Leguizamon <mleguizamon@byupathway.edu>
2026-01-23 01:18:42 -0300

    links update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -147,3 +147,11 @@ function displayForecast(data) {
                             <p>${tomorrow}: ${data.list[10].main.temp}&deg;C</p>
                             <p>${afterTomorrow}: ${data.list[18].main.temp}&deg;C</p>`;
 }
+
+const currentPage = window.location.pathname;
+const navLinks = document.querySelector('nav a');
+navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+        link.parentElement.classList.add('current');
+    }
+});
