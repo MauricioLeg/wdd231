58e1652936807714c04db8580710157f25e46f32
Mauricio Leguizamon <mleguizamon@byupathway.edu>
2026-01-23 00:42:23 -0300

    Home page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -18,38 +18,132 @@ async function createCards() {
     await fetch('data/members.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        data.forEach(company => {
-            const card = document.createElement("section");
-            card.classList.add('card-box');
-            let imgTag = `<img src="${company.logo}" width="150" height="auto loading="lazy" alt="Site's logo">`;
-            if (company.logo === "https://cdn.logo.com/hotlink-ok/logo-social.png") {
-                imgTag = `<img src="${company.logo}" width="190" height="100" style="aspect-ratio: 1.9; object-fit: contain;" loading="lazy" alt="Site's logo">`;
-            }
-            card.innerHTML = `${imgTag}
-            <h3>Company name: ${company.companyName}</h3>
-            <p>Address: ${company.address}</p>
-            <p>Phone number: ${company.phone}</p>
-            <p>Website: ${company.website}</p>
-            <p>Membership Level: ${company.membershipLevel}</p>`;
-        
-            cards.appendChild(card);
-        });
+        const isHomePage = document.body.classList.contains('home-page');
+        const companiesToShow = isHomePage
+        ? data.filter(c => c.membershipLevel === 2 || c.membershipLevel === 3).slice(0, 3) : data;
+        console.log(companiesToShow);
+        const isDirectory = document.body.classList.contains('directory');
+        const companiesDirectory = isDirectory 
+        ? data : data;
+        console.log(companiesDirectory);
+        if (document.body.classList.contains('home-page')) {
+            companiesToShow.forEach(company => {
+                const card = document.createElement("aside");
+                card.classList.add('card-box');
+                let imgTag = `<img src="${company.logo}" width="150" height="auto" loading="lazy" alt="Site's logo">`;
+                if (company.logo === "https://cdn.logo.com/hotlink-ok/logo-social.png") {
+                    imgTag = `<img src="${company.logo}" width="190" height="100" style="aspect-ratio: 1.9; object-fit: contain;" loading="lazy" alt="Site's logo">`;
+                }
+                card.innerHTML = `${imgTag}
+                <h3>${company.companyName}</h3>
+                <h4>Membership Level: ${company.membershipLevel}</h4>
+                <p>Address: ${company.address}</p>
+                <p>Phone number: ${company.phone}</p>
+                <p>Website: <a href="${company.website}" target="_blank">${company.companyName} Website</a></p>`;
+                
+                cards.appendChild(card);
+            })
+        }
+        if (document.body.classList.contains('directory')) {
+            companiesDirectory.forEach(company => {
+                const card = document.createElement("section");
+                card.classList.add('card-box');
+                let imgTag = `<img src="${company.logo}" width="150" height="auto" loading="lazy" alt="Site's logo">`;
+                if (company.logo === "https://cdn.logo.com/hotlink-ok/logo-social.png") {
+                    imgTag = `<img src="${company.logo}" width="190" height="100" style="aspect-ratio: 1.9; object-fit: contain;" loading="lazy" alt="Site's logo">`;
+                }
+                card.innerHTML = `${imgTag}
+                <h3>Company name: ${company.companyName}</h3>
+                <p>Address: ${company.address}</p>
+                <p>Phone number: ${company.phone}</p>
+                <p>Website: ${company.website}</p>
+                <p>Membership Level: ${company.membershipLevel}</p>`;
+            
+                cards.appendChild(card);
+            });
+        }
     })
     .catch(error => {
         console.error('Error fetching members:', error)
     });
 }
 
-createCards();
+document.addEventListener('DOMContentLoaded', () => {
+    createCards();
+    const display1 = document.querySelector("#display-btn1");
+    if (display1){
 
-const display1 = document.querySelector("#display-btn1");
-display1.addEventListener('click', () => {
-    cards.classList.add('grid');
-    cards.classList.remove('list');
+        display1.addEventListener('click', () => {
+            cards.classList.add('grid');
+            cards.classList.remove('list');
+        });
+    }
+
+    const display2 = document.querySelector("#display-btn2");
+    if (display2) {
+        display2.addEventListener('click', () => {
+            cards.classList.toggle('list');
+            cards.classList.remove('grid');
+        });
+    }
 });
-const display2 = document.querySelector("#display-btn2");
-display2.addEventListener('click', () => {
-    cards.classList.toggle('list');
-    cards.classList.remove('grid');
-});
+
+// HOME PAGE FUNCTIONALITY
+const currentWeather = document.querySelector('#current-weather');
+const forecast = document.querySelector('#fore-weather');
+
+const lat = -26.805264223692863;
+const lon = -65.21837586127;
+const apiKey = 'a03eb209e95979561eb6abdffa9c139e';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&&appid=${apiKey}`;
+const foreUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        const response2 = await fetch(foreUrl);
+        if (response.ok && response2.ok) {
+            let data = await response.json();
+            console.log(data);
+            displayCurrentWeather(data);
+            let data2 = await response2.json();
+            console.log(data2);
+            displayForecast(data2);
+        } else {
+            throw Error (await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayCurrentWeather(data) {
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    currentWeather.innerHTML = `<h2>Current Weather</h2>
+                                <img 
+                                src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"
+                                srcset="
+                                https://openweathermap.org/img/wn/${data.weather[0].icon}.png 1x,
+                                https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png 2x"
+                                width="100"
+                                height="100"
+                                alt="${data.weather[0].description}">
+                                <p>Temperature: ${data.main.temp}&deg;C</p>
+                                <p>${data.weather[0].description}
+                                <p>High: ${data.main.temp_max}</p>
+                                <p>Low: ${data.main.temp_min}</p>
+                                <p>Sunrise: ${data.sys.sunrise}</p>
+                                <p>Sunset: ${data.sys.sunset}</p>`;
+}
+function displayForecast(data) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const today = new Date();
+    const tomorrow = weekDays[(today.getDay() + 1) % 7];
+    const afterTomorrow = weekDays[(today.getDay() + 2) % 7];
+    forecast.innerHTML = `<h2>Weather Forecast</h2>
+                            <p>Today: ${data.list[1].main.temp}&deg;C</p>
+                            <p>${tomorrow}: ${data.list[10].main.temp}&deg;C</p>
+                            <p>${afterTomorrow}: ${data.list[18].main.temp}&deg;C</p>`;
+}
